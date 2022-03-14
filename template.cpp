c17e0d4c19202b17bf35b9144f73f57db608f7c7
diego <defsdiego@gmail.com>
2022-03-13 20:16:23 -0700

    template para .cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+Problema # 1 (pág 55):
+aaa
+aaa
+*/
+
+// @Autor:  @LaloFl
+// @Fecha:  13-03-2022
+
+using namespace std;
+
+int func()
+{
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
