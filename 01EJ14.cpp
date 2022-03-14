fd05586e8817636e610506678133d978d9e7d2ae
diego <defsdiego@gmail.com>
2022-03-13 20:16:49 -0700

    ej14 (wip)

---
+++ 01EJ14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*
+Problema # 14 (pág 55):
+Introducir las longitudes de la hipotenusa y un cateto de un triángulo rectángulo. Determinar el seno, coseno y tangente de cualquiera de los ángulos agudos del triángulo.
+*/
+
+// @Autor:  @LaloFl
+// @Fecha:  13-03-2022
+
+using namespace std;
+
+int func()
+{
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
